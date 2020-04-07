d45a37c2b121d2c1226f3dc9cfb52e195977de84
shockelectro <43023461+shockelectro@users.noreply.github.com>
2020-04-06 23:34:50 -0300

    Initial commit

---
+++ app/src/main/java/com/electroshock/mlsearch/MainActivity.java
@@ -0,0 +1,74 @@
+package com.electroshock.mlsearch;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.electroshock.mlsearch.consulta.ConsultaActivity;
+import com.electroshock.mlsearch.sqlite.DatabaseAdapter;
+
+import static com.electroshock.mlsearch.R.layout.activity_main;
+
+
+public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase db;
+    DatabaseAdapter dba;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main); //Error
+
+        dba = new DatabaseAdapter(getApplicationContext());
+
+        //ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"bd_items",null,1);
+        //db=conn.getWritableDatabase();
+
+    }
+
+    public void onClick(View view) {
+        Intent miIntent=null;
+        switch (view.getId()){
+            // TODO btnListaBusquedas completar y revisar
+            case R.id.btnListaBusquedas:
+                miIntent=new Intent(MainActivity.this,ConsultaActivity.class);
+                break;
+            case R.id.btnRegistroBusqueda:
+                miIntent=new Intent(MainActivity.this,RegistroBusquedaActivity.class);
+                break;
+
+                /* TODO Implementar botones de menu principal
+            case R.id.btnConsultaIndividual:
+                miIntent=new Intent(MainActivity.this,ConsultaActivity.class);
+                break;
+            case R.id.btnConsultaSpinner:
+                miIntent=new Intent(MainActivity.this,ConsultaActivity.class);
+                break;
+            case R.id.btnConsultaLista:
+                miIntent=new Intent(MainActivity.this,ConsultaActivity.class);
+                break;
+            case R.id.btnConsultaListaMascota:
+                miIntent=new Intent(MainActivity.this,ConsultaActivity.class);
+                break;
+            case R.id.btnConsultaListaPersonasRecycler:
+                miIntent=new Intent(MainActivity.this,ConsultaActivity.class);
+                break;
+                */
+        }
+        if (miIntent!=null){
+            startActivity(miIntent);
+        }
+
+    }
+
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+
+        // Close The Database
+        dba.close();
+    }
+
+}
